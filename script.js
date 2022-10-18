d3b72f02d332f5df77d7c50dd30dc5f0b7076b63
Riley <94302477+Rilaey@users.noreply.github.com>
2022-10-18 18:42:43 -0400

    completed html and css files. wrote variables in the js

---
+++ script.js
@@ -0,0 +1,11 @@
+// My global variables
+
+const timer = document.querySelector('.watch .time')
+const startBtn = document.getElementById('start');
+const stopBtn = document.getElementById('stop');
+const resetBtn = document.getElementById('reset');
+
+let counterSeconds = 0;
+let interval = null;
+
+
